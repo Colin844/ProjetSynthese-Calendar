ed9b91602cbe9324a0f84723d3bd54875eb5210e
your name <you@example.com>
2025-05-04 18:34:05 -0400

    [FONCTIONNALITÉ] : ajout du middleware checkAuth pour protéger les routes avec JWT

---
+++ backend/middleware/auth-check.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import HttpError from "../util/http-error.js";
+import User from "../models/user.js";
+
+const checkAuth = async (req, res, next) => {
+  try {
+    if (req.method === "OPTIONS") {
+      return next();
+    }
+
+    // Extraction du token
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      throw new Error("Authentification échouée (token manquant) !");
+    }
+
+    // Vérification du token et décodage
+    const decodedToken = jwt.verify(token, "codeSecret....");
+    const userId = decodedToken.id;
+
+    // Vérifie que l'utilisateur existe
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error("Utilisateur introuvable.");
+    }
+
+    // Ajoute l'utilisateur à la requête
+    req.user = user;
+
+    // Authorisation réussie, On continue
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentification échouée !", 401);
+    return next(error);
+  }
+};
+
+export default checkAuth;
