8e625bf842b86f3485d83b427dbbafbbb18d0659
Colin Leblanc <colin.leblanc5@gmail.com>
2025-05-08 13:23:37 -0400

    Implémentation composant Header & Création route dans App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+import { AuthContextProvider } from "./components/authContext/AuthContextProvider";
 import Calendar from "./components/Calendar/Calendar";
+import Header from "./components/header/Header";
 import "./App.css";
 
 function App() {
+  const routerLogout = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <>
+          <Header />
+          <Outlet />
+        </>
+      ),
+      children: [{ path: "/", element: <Calendar /> }],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <Calendar />
-    </div>
+    <AuthContextProvider>
+      <RouterProvider router={routerLogout} />
+    </AuthContextProvider>
   );
 }
 export default App;
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../authContext/AuthContext";
+
+const Header = () => {
+  const { isLoggedin, userId, logout } = useContext(AuthContext);
+
+  return (
+    <header className="header">
+      <h1>CAL3ND4R</h1>
+
+      <nav className="header__nav">
+        <Link to="/">Accueil</Link>
+        {isLoggedin && <Link to="/dashboard">Tableau des événements</Link>}
+        {!isLoggedin && <Link to="/login">Connexion</Link>}
+        {isLoggedin && (
+          <>
+            <span>Bienvenue, {userId}</span>
+            <button onClick={logout}>Déconnexion</button>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
